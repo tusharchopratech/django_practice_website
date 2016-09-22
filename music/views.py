cd0e3864305d2f529796da66f0e10da2744ef1c0
Tushar Chopra <tusharchopratech@gmail.com>
2016-09-22 13:12:15 +0530

    Initial commit

---
+++ music/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from .models import Album
+
+
+def index(request):
+    all_albums = Album.objects.all()
+
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href=' + url + '>' + album.album_title + '</a><br>'
+    return HttpResponse(html)
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album Id : " + str(album_id) + "</h2>")
