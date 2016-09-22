2c8029e156d89c63bccdabbf26319af2b6838671
Tushar Chopra <tusharchopratech@gmail.com>
2016-09-22 15:21:02 +0530

    render used - template removed

--- music/views.py
+++ music/views.py
@@ -1,15 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Album
 
 
 def index(request):
     all_albums = Album.objects.all()
+    context = {'all_albums': all_albums}
 
-    html = ''
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href=' + url + '>' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    return render(request, 'music/index.html', context)
 
 
 def detail(request, album_id):
